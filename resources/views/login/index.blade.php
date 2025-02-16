db1b5f37bb8fae0d1ef8e43b626842c47525b186
kasanojunior <kasanojunior@gmail.com>
2025-02-16 11:23:59 +0300

    hello

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -50,7 +50,7 @@
             </div>
             <div class="signup-prompt">
                 <span>Don't have an account? </span>
-                <a href="{{route('register')}}">Create one now!</a>
+                <a href="{{route('register')}}">Create Account</a>
             </div>
         </div>
     </div>
